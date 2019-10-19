d3c346f11980fe5e41578aa66e96685f3969366b
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-10-20 00:03:19 +0700

    Code formatting

--- nn.py
+++ nn.py
@@ -1,16 +1,20 @@
 import numpy as np
 import itertools
 
+
 def pairwise(iterable):
     a, b = itertools.tee(iterable)
     next(b, None)
     return zip(a, b)
 
+
 def relu(x):
     return x if x > 0 else 0
 
+
 def relu_derivative(x):
     return 1 if x > 0 else 0
+
 
 class NN:
     def __init__(self, layers, random_state=None):
@@ -21,8 +25,10 @@ class NN:
         for i, size in enumerate(pairwise(layers)):
             self.weights[i] = self.rng.rand(*size)
 
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
