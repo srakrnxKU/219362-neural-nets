b913f28ee966966998d5cc557edc375e7e714857
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-10-20 00:32:42 +0700

    Testing random behaviours

--- tests/class_test.py
+++ tests/class_test.py
@@ -20,3 +20,17 @@ class TestNN:
         x = np.array([[1, 1, 1], [2, 2, 2]])
         output = self.nn_1.forward(x)
         assert output.shape == (2, 5)
+
+
+class TestNNBehaviour:
+    nn_1 = nn.NN([3, 4, 5], random_state=1)
+    nn_2 = nn.NN([3, 4, 5], random_state=1)
+    nn_3 = nn.NN([3, 4, 5], random_state=3)
+
+    def test_random_state(self):
+        print(self.nn_1.weights[0])
+        for i in range(2):
+            np.testing.assert_equal(self.nn_1.weights[i], self.nn_2.weights[i])
+        for i in range(2):
+            with pytest.raises(AssertionError):
+                np.testing.assert_equal(self.nn_1.weights[i], self.nn_3.weights[i])
